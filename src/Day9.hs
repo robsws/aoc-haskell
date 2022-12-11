71fa0f71975d0f05433d7ff57c1a099c2d45598d
Rob Streeting <robsws@gmail.com>
2022-12-11 17:42:33 +0000

    feat(day9): part2

--- src/Day9.hs
+++ src/Day9.hs
@@ -7,7 +7,7 @@ import Debug.Trace (trace)
 
 type Visited = Set.Set Coord
 data Movement = MoveLeft | MoveRight | MoveUp | MoveDown deriving (Show)
-data RopeState = RopeState Coord Coord Visited deriving (Show)
+data RopeState = RopeState [Coord] Visited deriving (Show)
 
 parseInputs :: [String] -> [Movement]
 parseInputs [] = []
@@ -28,53 +28,72 @@ doMoves (hMove:tMoves) state =
         in doMoves tMoves state'
 
 moveHead :: Movement -> RopeState -> RopeState
-moveHead MoveLeft (RopeState (Coord hx hy) ropeTail visited) =
-    moveTail (RopeState (Coord (hx-1) hy) ropeTail visited)
-moveHead MoveRight (RopeState (Coord hx hy) ropeTail visited) =
-    moveTail (RopeState (Coord (hx+1) hy) ropeTail visited)
-moveHead MoveUp (RopeState (Coord hx hy) ropeTail visited) =
-    moveTail (RopeState (Coord hx (hy-1)) ropeTail visited)
-moveHead MoveDown (RopeState (Coord hx hy) ropeTail visited) =
-    moveTail (RopeState (Coord hx (hy+1)) ropeTail visited)
+moveHead _ (RopeState [] _) = error ("Rope is empty, this base case should never be reached.")
+moveHead MoveLeft (RopeState ((Coord hx hy):tRope) visited) =
+    moveTail (RopeState ((Coord (hx-1) hy):tRope) visited)
+moveHead MoveRight (RopeState ((Coord hx hy):tRope) visited) =
+    moveTail (RopeState ((Coord (hx+1) hy):tRope) visited)
+moveHead MoveUp (RopeState ((Coord hx hy):tRope) visited) =
+    moveTail (RopeState ((Coord hx (hy-1)):tRope) visited)
+moveHead MoveDown (RopeState ((Coord hx hy):tRope) visited) =
+    moveTail (RopeState ((Coord hx (hy+1)):tRope) visited)
 
 moveTail :: RopeState -> RopeState
-moveTail state@(RopeState ropeHead@(Coord hx hy) (Coord tx ty) visited)
-    -- Tail is too far directly up or down of head
+moveTail (RopeState [] _) = error ("Rope is empty, this base case should never be reached.")
+moveTail (RopeState (knot:[]) visited) =
+    -- If this is the last knot in the rope (the tail),
+    -- update the visited set with the current coordinates
+    RopeState [knot] (Set.insert knot visited)
+moveTail state@(RopeState (parentKnot@(Coord px py):(Coord kx ky):rope) visited)
+    -- Knot is too far directly up or down of parent
     | distance > 1 && xVec == 0 =
-        -- Move tail towards head up or down
-        let ropeTail' = Coord tx (ty+yDir)
-            in (RopeState ropeHead ropeTail' (Set.insert ropeTail' visited))
-    -- Tail is too far directly left or right of head
+        -- Move knot towards parent up or down
+        let knot' = Coord kx (ky+yDir)
+            (RopeState rope' visited') = moveTail (RopeState (knot':rope) visited)
+                in (RopeState (parentKnot:rope') visited')
+    -- Knot is too far directly left or right of head
     | distance > 1 && yVec == 0 =
         -- Move tail towards head left or right
-        let ropeTail' = Coord (tx+xDir) ty
-            in (RopeState ropeHead ropeTail' (Set.insert ropeTail' visited))
-    -- Tail is too far directly up or down of head and also one to left or right
+        let knot' = Coord (kx+xDir) ky
+            (RopeState rope' visited') = moveTail (RopeState (knot':rope) visited)
+                in (RopeState (parentKnot:rope') visited')
+    -- Knot is too far directly up or down of head and also one to left or right
     | distance > 2 && yMag > xMag =
         -- Move tail towards head up or down and back in line horizontally with head
-        let ropeTail' = Coord hx (ty+yDir)
-            in (RopeState ropeHead ropeTail' (Set.insert ropeTail' visited))
-    -- Tail is too far directly left or right of head and also one up or down
+        let knot' = Coord px (ky+yDir)
+            (RopeState rope' visited') = moveTail (RopeState (knot':rope) visited)
+                in (RopeState (parentKnot:rope') visited')
+    -- Knot is too far directly left or right of head and also one up or down
     | distance > 2 && xMag > yMag =
         -- Move tail towards head left or right and back in line vertically with head
-        let ropeTail' = Coord (tx+xDir) hy
-            in (RopeState ropeHead ropeTail' (Set.insert ropeTail' visited))
+        let knot' = Coord (kx+xDir) py
+            (RopeState rope' visited') = moveTail (RopeState (knot':rope) visited)
+                in (RopeState (parentKnot:rope') visited')
+    -- Knot is diagonally one too far away from parent
+    | distance > 3 =
+        -- Move knot diagonally towards parent
+        let knot' = Coord (kx+xDir) (ky+yDir)
+            (RopeState rope' visited') = moveTail (RopeState (knot':rope) visited)
+                in (RopeState (parentKnot:rope') visited')
     -- Tail is neighbouring head, so don't move it
     | otherwise = state
-    where xVec = (hx-tx)
+    where xVec = (px-kx)
           xMag = abs xVec
           xDir = xVec `div` xMag
-          yVec = (hy-ty)
+          yVec = (py-ky)
           yMag = abs yVec
           yDir = yVec `div` yMag
           distance = (abs xVec) + (abs yVec)
 
+countRopeTailCoordsVisited :: [String] -> Int -> Int
+countRopeTailCoordsVisited moves ropeLength = 
+    let initialState = RopeState (replicate ropeLength (Coord 0 0)) (Set.singleton (Coord 0 0))
+        moves' = parseInputs moves
+        RopeState _ visited = doMoves moves' initialState
+            in length . Set.toList $ visited
+
 part1 :: [String] -> String
-part1 inputs =
-    let initialState = RopeState (Coord 0 0) (Coord 0 0) (Set.singleton (Coord 0 0))
-        moves = parseInputs inputs
-        RopeState _ _ visited = doMoves moves initialState
-            in show . length . Set.toList $ visited
+part1 inputs = show $ countRopeTailCoordsVisited inputs 2
 
 part2 :: [String] -> String
-part2 inputs = ""
+part2 inputs = show $ countRopeTailCoordsVisited inputs 10
