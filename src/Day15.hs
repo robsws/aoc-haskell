24eed54c5c10faca6e999462b07094174e43fb5b
Rob Streeting <robsws@gmail.com>
2022-12-22 17:45:08 +0000

    feat(day15): part 2

--- src/Day15.hs
+++ src/Day15.hs
@@ -1,8 +1,11 @@
 module Day15 (part1, part2) where
 
-import Debug.Trace ( trace )
 import Data.Set (Set)
 import qualified Data.Set as Set
+import Control.Monad.State
+    ( MonadState(get, state), runState, State )
+import Data.List ( sort, find )
+import Data.Maybe ( isJust )
 
 import Parsing ( getAllIntsOnLine )
 import Common (getElemAt)
@@ -40,6 +43,33 @@ coveredOnRow sensorInfos beacons maxX x y acc
         else
             coveredOnRow sensorInfos beacons maxX (x+1) y acc
 
+addRange :: (Int, Int) -> State [(Int, Int)] ()
+addRange r = state $ \s -> ((), r:s)
+
+getSensorRangeOnRow :: SensorInfo -> Int -> Maybe (Int, Int)
+getSensorRangeOnRow (SensorInfo (Coord sx sy) _ range) y =
+    let remainderForX = range - abs (sy - y)
+    in if remainderForX > 0 then
+           Just (sx-remainderForX, sx+remainderForX)
+       else
+           Nothing
+
+findGapInRanges :: [(Int, Int)] -> Int -> Int -> Int -> Maybe Coord
+findGapInRanges [] _ _ _ = Nothing
+findGapInRanges ((s, e):rs) minX maxX y
+    | s > (minX+1) = Just $ Coord (minX+1) y
+    | otherwise = findGapInRanges rs (max minX e) maxX y
+
+gapOnRow :: [SensorInfo] -> Int -> Int -> State [(Int, Int)] (Maybe Coord)
+gapOnRow [] y maxX = do
+    ranges <- get
+    return $ findGapInRanges (sort ranges) 0 maxX y
+gapOnRow (sensorInfo:sensorInfos) y maxX = do
+    case getSensorRangeOnRow sensorInfo y of
+        Nothing -> return ()
+        Just range -> addRange range
+    gapOnRow sensorInfos y maxX
+
 part1 :: [String] -> Either String String
 part1 inputs = 
     case (sequence $ map parseLine inputs) of
@@ -50,4 +80,14 @@ part1 inputs =
                             in Right $ show $ coveredOnRow sensorInfos beacons maxX minX 2000000 0
 
 part2 :: [String] -> Either String String
-part2 inputs = Left ""
+part2 inputs = 
+    case (sequence $ map parseLine inputs) of
+        Nothing -> Left "Parsing error"
+        Just sensorInfos ->
+            let maxX = 4000000
+                gaps = map (\y -> fst $ runState (gapOnRow sensorInfos y maxX) []) [0..maxX]
+                gap = find isJust gaps
+            in case gap of
+                Nothing -> Left "No gap found."
+                Just (Just (Coord gx gy)) -> return $ show $ gx * 4000000 + gy
+                _ -> error ("Somehow ended up with no Just gap passing isJust")
